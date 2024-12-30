8500e61e5d7d3c439b0bdc8a29e2043bc6045c91
zhenwu.li <zhenwu.li@pixocial.com>
2024-12-30 18:01:23 +0800

    feat: json serialize

--- src/http.rs
+++ src/http.rs
@@ -1,7 +1,8 @@
 use wasm_bindgen::prelude::*;
 use reqwest;
 use web_sys::{console, window};
-
+use serde_json::{self, Value};
+use serde_wasm_bindgen::{self};
 
 fn guarantee_http_url(url: &str) -> String {
     if url.starts_with("http://") || url.starts_with("https://") {
@@ -11,8 +12,15 @@ fn guarantee_http_url(url: &str) -> String {
     format!("{}{}", current_host, url)
 }
 
+fn serialize_resp(value: &String) -> Result<JsValue, JsError> {
+    // let serializer: Serializer = Serializer::new().serialize_maps_as_objects(true);
+    let data: Value = serde_json::from_str(value).map_err(|e| JsError::new(&format!("JSON parse error: {}", e)))?;
+
+    Ok(serde_wasm_bindgen::to_value(&data)?)
+}
+
 #[wasm_bindgen(js_name=get)]
-pub async fn fetch_with_reqwest(mut url: String) -> Result<String, JsValue> {
+pub async fn fetch_with_reqwest(mut url: String) -> Result<JsValue, JsError> {
     url = guarantee_http_url(&url);
     
     let response = reqwest::Client::new().get(&url)
@@ -22,44 +30,25 @@ pub async fn fetch_with_reqwest(mut url: String) -> Result<String, JsValue> {
             console::log_1(&JsValue::from_str(&e.to_string()));
             JsError::from(&e)
         })?;
-
-        // .map_err(|e| JsError::from(&e))?;
         
     let text = response.text()
-        .await
-        .map_err(|e| JsValue::from_str(&e.to_string()))?;
-    
-    Ok(text)
+        .await?;
+
+    serialize_resp(&text)
 }
 // POST 请求示例
 #[wasm_bindgen(js_name=post)]
-pub async fn post_with_reqwest(url: String, body: String) -> Result<String, JsValue> {
+pub async fn post_with_reqwest(url: String, body: String) -> Result<JsValue, JsError> {
     let url = guarantee_http_url(&url);
     let client = reqwest::Client::new();
     let response = client.post(&url)
         .header("Content-Type", "application/json")
         .body(body)
         .send()
-        .await
-        .map_err(|e| JsError::from(&e))?;
+        .await?;
         
     let text = response.text()
-        .await
-        .map_err(|e| JsValue::from_str(&e.to_string()))?;
-        
-    Ok(text)
-}
+        .await?;
 
-#[wasm_bindgen(js_name=getGithub)]
-pub async fn run() -> Result<JsValue, JsValue> {
-    let res = reqwest::Client::new()
-        .get("https://api.github.com/repos/rustwasm/wasm-bindgen/branches/main")
-        .header("Accept", "application/vnd.github.v3+json")
-        .send()
-        .await
-        .map_err(|e| JsError::from(&e))?;
-    
-    let text = res.text().await.map_err(|e| JsError::from(&e))?;
-    // let branch_info: Branch = serde_json::from_str(&text).unwrap();
-    Ok(JsValue::from_str(&text))
+    serialize_resp(&text)
 }
